a420e030ea54035f9d1aa238f04c12af9227cc3c
gdepuydt <geert_depuydt@hotmail.com>
2019-01-06 19:55:14 +0100

    we have a window

--- src/i4trender.cpp
+++ src/i4trender.cpp
@@ -1,10 +1,24 @@
 // i4trender.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-
 #include <stdio.h>
+#include <assert.h>
+#include <GLFW/glfw3.h>
+
+
 
 int main()
 {
 	printf("Hello Vulkan gfx universe!\n");
+
+	assert(glfwInit());
+
+	GLFWwindow *window = glfwCreateWindow(1024, 768, "i4t_Vulkan", 0, 0);
+	assert(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+
+
 }
