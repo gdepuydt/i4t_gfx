690d6da7646dc264820d1ea091390f703da9bc88
gdepuydt <geert_depuydt@hotmail.com>
2019-01-06 14:13:53 +0100

    VS project setup

---
+++ src/i4trender.cpp
@@ -0,0 +1,9 @@
+// i4trender.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello Vulkan gfx universe!\n");
+}
