84a8f8fc6795e6cbd224061be1cc8f329867d36a
gdepuydt <geert_depuydt@hotmail.com>
2019-01-09 22:11:21 +0100

    continued preparing for image on screen

--- src/i4trender.cpp
+++ src/i4trender.cpp
@@ -122,6 +122,33 @@ VkSurfaceKHR createSurface(VkInstance instance, GLFWwindow *window) {
 #endif
 }
 
+VkSwapchainKHR createSwapchain(VkDevice device, VkSurfaceKHR surface, uint32_t width, uint32_t height, uint32_t familyIndex) {
+	VkSwapchainCreateInfoKHR createInfo = { VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
+	createInfo.surface = surface;
+	createInfo.minImageCount = 2;
+	createInfo.imageFormat = VK_FORMAT_R8G8B8A8_UNORM; //Shortcut: some devices BGRA, not RGB, you need to find this out...
+	createInfo.imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+	createInfo.imageExtent.width = width;
+	createInfo.imageExtent.height = height;
+	createInfo.imageArrayLayers = 1;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	createInfo.queueFamilyIndexCount = 1;
+	createInfo.pQueueFamilyIndices = &familyIndex;
+	createInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+
+	VkSwapchainKHR swapchain = 0;
+	VK_CHECK(vkCreateSwapchainKHR(device, &createInfo, 0, &swapchain));
+	return swapchain;
+}
+
+
+VkSemaphore createSemaphore(VkDevice device) {
+	VkSemaphoreCreateInfo createInfo = { VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+	VkSemaphore semaphore = 0;
+	VK_CHECK(vkCreateSemaphore(device, &createInfo, 0, &semaphore));
+	return semaphore;
+}
+
 int main()
 {
 	assert(glfwInit());
@@ -147,36 +174,22 @@ int main()
 	VkSurfaceKHR surface = createSurface(instance, window);
 	assert(surface);
 
-	VkSwapchainCreateInfoKHR createInfo = { VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
-	//TODO --->Here's where we are!
-	/*VkSwapchainCreateFlagsKHR        flags;
-	VkSurfaceKHR                     surface;
-	uint32_t                         minImageCount;
-	VkFormat                         imageFormat;
-	VkColorSpaceKHR                  imageColorSpace;
-	VkExtent2D                       imageExtent;
-	uint32_t                         imageArrayLayers;
-	VkImageUsageFlags                imageUsage;
-	VkSharingMode                    imageSharingMode;
-	uint32_t                         queueFamilyIndexCount;
-	const uint32_t*                  pQueueFamilyIndices;
-	VkSurfaceTransformFlagBitsKHR    preTransform;
-	VkCompositeAlphaFlagBitsKHR      compositeAlpha;
-	VkPresentModeKHR                 presentMode;
-	VkBool32                         clipped;
-	VkSwapchainKHR                   oldSwapchain;*/
+	int windowWidth = 0, windowHeight = 0;
+	glfwGetWindowSize(window, &windowWidth, &windowHeight);
+	VkSwapchainKHR swapchain = createSwapchain(device, surface, windowWidth, windowHeight, familyIndex);
+	assert(swapchain);
 
-	VkSwapchainKHR swapchain = 0;
-	VK_CHECK(vkCreateSwapchainKHR(device, &createInfo, 0, &swapchain));
+	VkSemaphore semaphore = createSemaphore(device);
+	assert(semaphore);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+		
+		uint32_t imageIndex = 0;
+
+		VK_CHECK(vkAcquireNextImageKHR(device, swapchain, ~0ull, semaphore, VK_NULL_HANDLE, &imageIndex));
+
 	}
-	
-	
 	glfwDestroyWindow(window);
 	vkDestroyInstance(instance, 0);
-
-
-
 }
